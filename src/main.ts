07f1f73c128ef42d3caf067d8feecdab828a05d6
Jean Pedro <jjeanpedro20@gmail.com>
2026-03-31 21:06:18 -0300

    blog_pessoal

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'localhost:4000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
+}
+bootstrap();
